a8e5cc62aa1443d1455385132c3db36dcdc23a55
Tasi <tasi@Ninos-MacBook-Pro.local>
2016-09-02 17:56:36 +0100

    xs

--- First app/ViewController.swift
+++ First app/ViewController.swift
@@ -15,7 +15,7 @@ class ViewController: UIViewController {
     
     
     @IBAction func buttonClicked(_ sender: AnyObject) {
-        theLabel.text = "Answer: \ (Double(text1.text!)! + Double(text2.text!)!)"
+        theLabel.text = "Answer: \(Double(text1.text!)! + Double(text1.text!)!)"
         }
     }
     
